7d5a0332138e10b5b0a22373c55ab70f8b788179
Luan Campos <luancamposproducoes@gmail.com>
2022-05-02 12:47:16 -0300

    configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,18 @@
-export default function Home() {
+import React from "react";
+
+function Home() {
   return (
-    <div>
-      <h1>MBTech Store</h1>
-    </div>
+    <>
+      MBTEch Store
+      <div className="products-heading">
+        <h2>Beset Selling Products</h2>
+        <p>Spearks of many variants</p>
+      </div>
+      <div>
+        {["product1", "product2", "product3"].map((product) => product)}
+      </div>
+    </>
   );
 }
+
+export default Home;
